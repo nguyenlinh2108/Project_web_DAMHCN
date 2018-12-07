e8ff8039dc5c8f4c21171809c6b3197b3978ebbb
= <=>
2018-12-07 22:48:14 +0700

    sửa password cho file config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
 $CONFIG_DATABASE['HOST'] = 'localhost';
 $CONFIG_DATABASE['SCHEMA'] = 'ban_hang';
 $CONFIG_DATABASE['USERNAME'] = 'root';
-$CONFIG_DATABASE['PASSWORD'] = "mysql";
+$CONFIG_DATABASE['PASSWORD'] = "";
 
 define("ADMIN_SALT_PASSWORD", "XinchaoVietNam2018");
 
