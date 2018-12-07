29546691360102cad702590c081c25acd6704163
= <=>
2018-12-07 22:43:46 +0700

    update list_slider cho admin

--- admin/list_slider.php
+++ admin/list_slider.php
@@ -1,10 +1,11 @@
-<?php include('../inc/myconnect.php'); ?>
-<?php include('../inc/function.php'); ?>
-<?php include('includes/header.php'); 
+
+<?php
+    include('includes/header.php');
+    require_once __DIR__ . "/../db/db.php";
 ?>
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-		<h3>Bài viết</h3>
+		<h3>Dánh sách ảnh Slider</h3>
 		<table class="table table-hover">
 			<thead>	
 				<tr>
@@ -17,6 +18,7 @@
 			</thead>
 			<tbody>
 				<?php
+                $db = db::getInstance();
 					//đặt số bản ghi cần hiện thị
 					$limit=10;
 					//Xác định vị trí bắt đầu
@@ -35,36 +37,37 @@
 					else
 					{
 						//Nếu p không có, thì sẽ truy vấn CSDL để tìm xem có bao nhiêu page
-						$query_pg="SELECT COUNT(id) FROM slide";
-						$results_pg=mysqli_query($dbc,$query_pg);
-						kt_query($results_pg,$query_pg);
-						list($record)=mysqli_fetch_array($results_pg,MYSQLI_NUM);						
-						//Tìm số trang bằng cách chia số dữ liệu cho số limit	
-						if($record > $limit)
-						{
-							$per_page=ceil($record/$limit);
-						}
+//						$query_pg="SELECT COUNT(id) FROM slide";
+
+						//Tìm số trang bằng cách chia số dữ liệu cho số limit
+                        if($db->select_one("SELECT COUNT(id) FROM slide"))
+                        {
+                            $total = $db->getResult()->{'COUNT(id)'};
+
+                            $per_page = ceil (intval($total)/$limit);
+                        }
 						else
 						{
 							$per_page=1;
 						}
 					}					
-					$query="SELECT * FROM slide ORDER BY id LIMIT {$start},{$limit}";
-					$results=mysqli_query($dbc,$query);
-					kt_query($results,$query);
-					while($slider=mysqli_fetch_array($results,MYSQLI_ASSOC))
-					{
-					?>
-					<tr>
-						<td><?php echo $slider['id']; ?></td>
-						<td><?php echo$slider['link']; ?></td>
-						<td><img width="100" src="../<?php echo $slider['image']; ?>"/></td>	
-						<td><a href="edit_slider.php?id=<?php echo $slider['id']; ?>"><img width="16" src="../images/icon_edit.png"></a></td>						
-						<td><a href="delete_slider.php?id=<?php echo $slider['id'];?>" onclick="return confirm('Bạn có thực sự muốn xóa không');"><img width="16" src="../images/icon_delete.png"></a></td>
-					</tr>
-					<?php		
-					}
+					$query="SELECT id, link, image FROM slide ORDER BY id LIMIT {$start},{$limit}";
+					if($db->select($query))
+                    {
+                        foreach ($db->getResult() as $obj)
+                        {
+                         ?>
+                            <tr>
+                                <td><?php echo $obj->id; ?></td>
+                                <td><?php echo$obj->link; ?></td>
+                                <td><img width="100px" src="../<?php echo $obj->image; ?>"/></td>
+                                <td><a href="edit_slider.php?id=<?php echo $obj->id; ?>"><img width="16" src="/public/images/icon_edit.png"></a></td>
+                                <td><a href="delete_slider.php?id=<?php echo $obj->id;?>" onclick="return confirm('Bạn có thực sự muốn xóa không');"><img width="16" src="/public/images/icon_delete.png"></a></td>
+                            </tr>
 
+                            <?php
+                        }
+					}
 				?>				
 			</tbody>
 		</table>
@@ -76,14 +79,14 @@
 				//Nếu không phải là trang đầu thì hiện thị trang trước
 				if($current_page !=1)
 				{
-					echo "<li><a href='list_baiviet.php?s=".($start - $limit)."&p={$per_page}'>Back</a></li>";
+					echo "<li><a href='list_slider.php?s=".($start - $limit)."&p={$per_page}'>Back</a></li>";
 				}
 				//hiện thị những phần còn lại của trang
 				for ($i=1; $i <= $per_page ; $i++) 
 				{ 
 					if($i != $current_page)
 					{
-						echo "<li><a href='list_baiviet.php?s=".($limit *($i - 1))."&p={$per_page}'>{$i}</a></li>";
+						echo "<li><a href='list_slider.php?s=".($limit *($i - 1))."&p={$per_page}'>{$i}</a></li>";
 					}
 					else
 					{
@@ -93,7 +96,7 @@
 				//Nếu không phải trang cuối thì hiện thị nút next
 				if($current_page != $per_page)
 				{
-					echo "<li><a href='list_baiviet.php?s=".($start + $limit)."&p={$per_page}'>Next</a></li>";	
+					echo "<li><a href='list_slider.php?s=".($start + $limit)."&p={$per_page}'>Next</a></li>";
 				}
 			}
 			echo "</ul>";			
