35733a07f29e00304dcb7ffe0671778451d8c8b9
= <=>
2018-12-08 10:20:09 +0700

    chỉnh sửa lời chào khi vào trang quản trị cho admin

--- admin/index.php
+++ admin/index.php
@@ -3,13 +3,14 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Danh sách <small>bình luận</small>
+                            Chào mừng bạn đến trang quản trị hệ thống của THE PODS
+<!--                            <small>bình luận</small>-->
                         </h1>
-                        <ol class="breadcrumb">
-                            <li class="active">
-                                <i class="fa fa-dashboard"></i> Dashboard
-                            </li>
-                        </ol>
+<!--                        <ol class="breadcrumb">-->
+<!--                            <li class="active">-->
+<!--                                <i class="fa fa-dashboard"></i> Dashboard-->
+<!--                            </li>-->
+<!--                        </ol>-->
                     </div>
                 </div>
                 <!-- /.row -->
