bd20c97cc203ae0e155f00554c3f1d37f187a25f
Long <alphahoai@gmail.com>
2018-12-08 21:17:47 +0700

    thêm chức năng thêm sản phẩm

--- admin/add_product.php
+++ admin/add_product.php
@@ -1,181 +1,199 @@
-<?php include('includes/header.php');?>
-<style type="text/css">
-.required
-{
-	color:red;
+<?php
+require_once __DIR__ . "/includes/header.php";
+require_once __DIR__ . "/../db/db.php";
+
+$db = db::getInstance();
+
+if ($db->select("SELECT * FROM type_product")) {
+    $types = $db->getResult();//Danh sách các loại sản phẩm
 }
-</style>
-<div class="row">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-		<?php 
-			include('../inc/images_helper.php');
-			include('../inc/function.php');
-			include('../inc/myconnect.php'); 
-			if($_SERVER['REQUEST_METHOD']=='POST')
-			{
-				$errors=array();
-				if($_POST['parent']==0)
-				{
-					$parent_id=0;
-				}
-				else
-				{
-					$parent_id=$_POST['parent'];	
-				}
-				if(empty($_POST['title']))
-				{
-					$errors[]='title';
-				}
-				else
-				{
-					$title=$_POST['title'];
-				}
-				$tomtat=$_POST['tomtat'];
-				$noidung=$_POST['noidung'];
-				if(empty($_POST['ordernum']))
-				{
-					$ordernum=0;
-				}
-				else
-				{
-					$ordernum=$_POST['ordernum'];
-				}
-				$status=$_POST['status'];
-				if(empty($errors))
-				{
-					//upload hình ảnh
-					if(($_FILES['img']['type']!="image/gif")
-						&&($_FILES['img']['type']!="image/png")
-						&&($_FILES['img']['type']!="image/jpeg")
-						&&($_FILES['img']['type']!="image/jpg"))
-					{
-						$message="File không đúng định dạng";	
-					}
-					elseif ($_FILES['img']['size']>1000000) 
-					{
-						$message="Kích thước phải nhỏ hơn 1MB";						
-					}
-					elseif ($_FILES['img']['size']=='') 
-					{
-						$message="Bạn chưa chọn file ảnh";
-					}
-					else
-					{
-						$img=$_FILES['img']['name'];
-						$link_img='upload/'.$img;
-						move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$img);																														
-						//Xử lý Resize, Crop hình anh
-						$temp=explode('.',$img);
-						if($temp[1]=='jpeg' or $temp[1]=='JPEG')
-						{
-							$temp[1]='jpg';
-						}
-						$temp[1]=strtolower($temp[1]);
-						$thumb='upload/resized/'.$temp[0].'_thumb'.'.'.$temp[1];
-						$imageThumb=new Image('../'.$link_img);
-						//Resize anh						
-						if($imageThumb->getWidth()>700)
-						{
-							$imageThumb->resize(700,'resize');
-						}												
-						$imageThumb->save($temp[0].'_thumb','../upload/resized');
-					}
-					$ngaydang_ht=explode("-",$_POST['ngaydang']);
-					$ngaydang_in=$ngaydang_ht[2].'-'.$ngaydang_ht[1].'-'.$ngaydang_ht[0];
-					$giodang_in=$_POST['giodang'];
-					//inser vao trong db
-					$query="INSERT INTO tblbaiviet(danhmucbaiviet,title,tomtat,noidung,anh,anh_thumb,ngaydang,giodang,ordernum,status)
-							VALUES($parent_id,'{$title}','{$tomtat}','{$noidung}','{$link_img}','{$thumb}','{$ngaydang_in}','{$giodang_in}',$ordernum,$status)";
-					$results=mysqli_query($dbc,$query);
-					kt_query($results,$query);
-					if(mysqli_affected_rows($dbc)==1)
-					{
-						echo "<p style='color:green;'>Thêm mới thành công</p>";
-					}
-					else
-					{
-						echo "<p style='required'>Thêm mới không thành công</p>";
-					}					
-					$_POST['title']='';
-				}
-				else
-				{
-					$message="<p class='required'>Bạn hãy nhập đầy đủ thông tin</p>";
-				}
-			}
-		?>
-		<form name="frmbaiviet" method="POST" enctype="multipart/form-data">
-			<?php 
-				if(isset($message))
-				{
-					echo $message;
-				}
-			?>
-			<h3>Thêm mới sản phẩm</h3>
-			<div class="form-group">
-				<label style="display:block;">Danh mục bài viết</label>
-				<?php selectCtrl('parent','forFormdim'); ?>
-			</div>
-			<div class="form-group">
-				<label>Tên</label>
-				<input type="text" name="title" value="<?php if(isset($_POST['name'])){ echo $_POST['name'];} ?>" class="form-control" placeholder="Tên">
-				<?php 
-					if(isset($errors) && in_array('title',$errors))
-					{
-						echo "<p class='required'>Bạn chưa nhập tên</p>";
-					}
-				?>
-			</div>
-			<div class="form-group">
-				<label>Tên</label>
-				<input type="text" name="title" value="<?php if(isset($_POST['name'])){ echo $_POST['name'];} ?>" class="form-control" placeholder="Tên">
-				<?php 
-					if(isset($errors) && in_array('title',$errors))
-					{
-						echo "<p class='required'>Bạn chưa nhập tên</p>";
-					}
-				?>
-			</div>
-			<div class="form-group">
-				<label style="display:block;">Tóm tắt</label>
-				<textarea name="tomtat" style="Width:100%;height:150px;"></textarea>
-			</div>
-			<div class="form-group">
-				<label style="display:block;">Nội dung</label>
-				<textarea id="noidung" name="noidung" style="Width:100%;height:150px;"></textarea>
-			</div>
-			<div class="form-group">
-				<label>Ảnh đại diện</label>
-				<input type="file" name="img" value="">
-			</div>
-			<div class="form-group">
-				<label>Ngày đăng</label>
-				<div id="datepicker" class="input-group date" data-date-format="dd-mm-yyyy"> 
-					<input class="form-control" readonly="true" type="text" name="ngaydang"> 
-					<span class="input-group-addon">
-						<i class="glyphicon glyphicon-calendar"></i>
-					</span> 
-				</div>
-			</div>
-			<div class="form-group">
-				<label>Giờ đăng</label>
-				<?php 
-					date_default_timezone_set('Asia/Ho_Chi_Minh');
-					$today=date('g:i A');
-				?>
-				<input type="text" name="giodang" value="<?php echo $today; ?>" class="form-control">
-			</div>
-			<div class="form-group">
-				<label>Thứ tự</label>
-				<input type="text" name="ordernum" value="<?php if(isset($_POST['ordernum'])){ echo $_POST['ordernum'];} ?>" class="form-control">
-			</div>
-			<div class="form-group">
-				<label style="display:block;">Trạng thái</label>
-				<label class="radio-inline"><input checked="checked" type="radio" name="status" value="1">Hiện thị</label>
-				<label class="radio-inline"><input type="radio" name="status" value="0">Không hiện thị</label>
-			</div>
-			<input type="submit" name="submit" class="btn btn-primary" value="Thêm mới">
-		</form>
-	</div>
-</div>
-<?php include('includes/footer.php');?>
+
+if ($db->select("SELECT * FROM unit")) {
+    $units = $db->getResult();//Danh sách các loại sản phẩm
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $message = "";
+    if (empty($_POST['name'])) {
+        $message .= "<p class='alert alert-danger'>Bạn hãy nhập thiếu tên sản phẩm</p>";
+    }
+    if (empty($_POST['type'])) {
+        $message .= "<p class='alert alert-danger'>Bạn hãy nhập thiếu loại sản phẩm</p>";
+    } else if (!is_numeric($_POST['type'])) {
+        $message .= "<p class='alert alert-danger'>Loại sản phẩm phải là số</p>";
+    }
+    if (empty($_POST['description'])) {
+        $message .= "<p class='alert alert-danger'>Bạn hãy nhập thiếu mô tả sản phẩm</p>";
+    }
+    if (empty($_POST['unit_price'])) {
+        $message .= "<p class='alert alert-danger'>Bạn hãy nhập thiếu giá sản phẩm</p>";
+    }
+    else if (!is_numeric($_POST['unit_price'])) {
+        $message .= "<p class='alert alert-danger'>Giá sản phẩm phải là số</p>";
+    }
+    if (empty($_POST['unit_id'])) {
+        $message .= "<p class='alert alert-danger'>Bạn hãy nhập thiếu unit id sản phẩm</p>";
+    } else if (!is_numeric($_POST['unit_id'])) {
+        $message .= "<p class='alert alert-danger'>unit id phải là số</p>";
+    }
+    if (empty($_POST['promotion_price'])) {
+        $message .= "<p class='alert alert-danger'>Bạn hãy nhập thiếu giá khuyến mãi của sản phẩm</p>";
+    } else if (!is_numeric($_POST['promotion_price'])) {
+        $message .= "<p class='alert alert-danger'>Giá khuyễn mãi phải là số</p>";
+    }
+
+    if ($message === "") {
+        if ($db->select_one("SELECT * FROM product WHERE name = '" . db::validSql($_POST['name'])
+            . "' AND description = '" . db::validSql($_POST['description']) . "' AND unit_price = '"
+            . db::validSql($_POST['unit_price']) . "'")) {
+            $message .= "<p class='alert alert-danger'>Sản phẩm đã tồn tại.</p>";
+        } else if ($db->insert("product", [
+            "name" => $_POST['name'],
+            "type" => $_POST['type'],
+            "description" => $_POST['description'],
+            "unit_price" => $_POST['unit_price'],
+            "unit_id" => $_POST['unit_id'],
+            "soluong" => $_POST['soluong'],
+            "promotion_price" => $_POST['promotion_price'],
+        ])) {
+            $message .= "<p class='alert alert-success'>Thêm thành công</p>";
+            unset($_POST);
+        } else $message .= "<p class='alert alert-danger'>Thêm thất bại</p>";
+    }
+}
+
+?>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <form name="form_product" id="form_product" method="POST" enctype="multipart/form-data">
+                <div id="message">
+                    <?php
+                    if (isset($message)) {
+                        echo $message;
+                    }
+                    ?>
+                </div>
+                <h3>Thêm mới sản phẩm</h3>
+
+                <div class="form-group">
+                    <label>Tên</label>
+                    <input type="text" name="name" value="<?php if (isset($_POST['name'])) echo $_POST['name']; ?>"
+                           class="form-control" placeholder="Tên">
+                </div>
+
+                <?php
+                if (isset($types) && is_array($types)) {
+                    ?>
+                    <div class="form-group">
+                        <label>Loại</label>
+                        <input type="text" hidden name="type" value="">
+                        <select id="select-type" class="selectpicker show-tick" data-width="auto">
+                            <?php
+                            foreach ($types as $type) {
+                                ?>
+                                <option value="<?= $type->id ?>"><?= $type->name ?></option>
+                                <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <?php
+                }
+                ?>
+
+                <?php
+                if (isset($units) && is_array($units)) {
+                    ?>
+                    <div class="form-group">
+                        <label>Unit</label>
+                        <input type="text" hidden name="unit_id" value="">
+                        <select id="select-unit" class="selectpicker show-tick" data-width="auto">
+                            <?php
+                            foreach ($units as $unit) {
+                                ?>
+                                <option value="<?= $unit->id ?>"><?= ucfirst($unit->name) ?></option>
+                                <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <?php
+                }
+                ?>
+                <div class="form-group">
+                    <label>Mô tả</label>
+                    <input type="text" name="description"
+                           value="<?php if (isset($_POST['description'])) echo $_POST['description']; ?>"
+                           class="form-control" placeholder="Mô tả">
+                </div>
+
+                <div class="form-group">
+                    <label>Giá</label>
+                    <input type="text" name="unit_price"
+                           value="<?php if (isset($_POST['unit_price'])) echo $_POST['unit_price']; ?>"
+                           class="form-control" placeholder="Giá mỗi đơn vị">
+                </div>
+
+                <div class="form-group">
+                    <label>Số lượng</label>
+                    <input type="text" name="soluong"
+                           value="<?php if (isset($_POST['soluong'])) echo $_POST['soluong']; ?>"
+                           class="form-control" placeholder="Số lượng">
+                </div>
+
+                <div class="form-group">
+                    <label>Giá khuyến mãi</label>
+                    <input type="text" name="promotion_price"
+                           value="<?php if (isset($_POST['promotion_price'])) echo $_POST['promotion_price']; ?>"
+                           class="form-control" placeholder="Giá khuyến mãi">
+                </div>
+
+                <div class="form-group">
+                    <label>Ảnh</label>
+                    <input type="file" name="img" value="">
+                </div>
+
+                <input type="submit" name="btnSubmit" class="btn btn-primary" value="Thêm mới">
+            </form>
+
+            <script>
+                $(document).ready(function () {
+                    $("#form_product input[name='btnSubmit']").click(function (event) {
+
+                        event.preventDefault();
+
+                        var isValidInput = true;
+                        $("#form_product .form-group:has(input[type='text'])").not( $(".form-group:has(.selectpicker)")).each(function () {
+
+                                let input = $(this).find("input[type='text']");
+                                let label = $(this).find("label").text();
+
+                                if (typeof input.val() === "string") {
+                                    $(this).find(".message").remove();
+                                    if (input.val() == null || input.val().trim() === "") {
+                                        isValidInput = false;
+                                        $(this).append("<p class='alert alert-danger message'>Không thể để trống trường " + label + "</p>");
+                                    } else if ((label === "Giá" || label === "Số lượng" || label === "Giá khuyến mãi") && !isUnsignedNumber(input.val())) {
+                                        isValidInput = false;
+                                        $(this).append("<p class='alert alert-danger message'>Bạn phải nhập " + label + " là số</p>");
+                                    }
+                                }
+
+                        });
+
+                        $("#form_product .form-group:has(.selectpicker)").each(function () {
+                            let selectpicker = $(this).find(".selectpicker").selectpicker('val');
+                            $(this).find("input").attr("value", selectpicker);
+                        });
+
+                        if (isValidInput) $('#form_product').submit();
+                    });
+                });
+
+                function isUnsignedNumber(str) {
+                    return /^\d+$/.test(str);
+                }
+            </script>
+        </div>
+    </div>
+<?php include('includes/footer.php'); ?>
