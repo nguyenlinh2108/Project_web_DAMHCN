b0198b584b1693d1326ac91696a0dda5437c71e0
Long <alphahoai@gmail.com>
2018-12-08 12:22:07 +0700

    cho khai báo jquery và bootstrap js lên đầu

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -35,18 +35,15 @@ if (isset($_SESSION['login']['success']) && $_SESSION['login']['success']) {
     <!-- Custom CSS -->
     <link href="./public/css/sb-admin.css" rel="stylesheet">
 
-    <!-- Morris Charts CSS -->
-    <link href="./public/css/plugins/morris.css" rel="stylesheet">
-
     <!-- Custom Fonts -->
     <link href="./public/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+
+    <script src="./public/js/jquery.js"></script>
+    <script src="./public/js/bootstrap.js"></script>
+
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
 
