ff81e7c75a65d3324404a85eb82cb6ee1ee2ccdf
= <=>
2018-12-10 10:01:22 +0700

    update: thêm nút thêm mới cho list_contact

--- admin/list_contact.php
+++ admin/list_contact.php
@@ -12,6 +12,7 @@ include "includes/header.php";
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <h3>Danh sách phản hồi</h3>
+        <a href="add_contact.php" class="btn btn-primary" style="float: right">Thêm mới</a>
         <table class="table table-hover">
             <thead>
             <tr>
