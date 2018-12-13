6321b1f252b2abf07b3a90e0f1089981d501e251
= <=>
2018-12-13 13:27:45 +0700

    update list_unit

--- admin/list_unit.php
+++ admin/list_unit.php
@@ -67,7 +67,7 @@ include "includes/header.php"
                         <td><?php echo $obj->id; ?></td>
                         <td><?php echo $obj->name; ?></td>
                         <td><a href="edit_unit.php?id=<?php echo $obj->id; ?>"><img width="16px" src="../public/images/icon_edit.png"></a></td>
-                        <td><a href="delete_unit.php?<?php echo $obj->id; ?>" onclick="return confirm('Bạn có thực sự muốn xóa không?')"><img width="16px" src="../public/images/icon_delete.png"></a></td>
+                        <td><a href="delete_unit.php?id=<?php echo $obj->id; ?>" onclick="return confirm('Bạn có thực sự muốn xóa không?')"><img width="16px" src="../public/images/icon_delete.png"></a></td>
                     </tr>
                     <?php
                 }
