a4303319cacabad940d6e8b5b7c4b2d6f08629b8
= <=>
2018-12-14 22:55:55 +0700

    them file edit type product

---
+++ admin/edit_type_product.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linh nguyễn
+ * Date: 12/14/2018
+ * Time: 9:55 PM
+ */
+ob_start();
+require_once __DIR__ . "/includes/header.php";
+require_once __DIR__ . "/../db/db.php";
+
+$db = db::getInstance();
+
+if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
+    $id = $_GET['id'];
+    if ($db->select_one("SELECT * FROM type_product WHERE id={$id}")) {
+        $name_db = $db->getResult()->name;
+        $description_db = $db->getResult()->description;
+        $img_db = $db->getResult()->image;
+    } else {
+        //Không có bản ghi nào có id như vậy
+        header('Location: list_type_product.php');
+        exit();
+    }
+} else {
+    header('Location: list_type_product.php');
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $message = "";
+    if (!isset($_POST['name'])) {
+        $message .= "<p class='alert alert-danger message'>Bạn đã nhập thiếu tên loại sản phẩm</p>";
+    }
+    if (!isset($_POST['description'])) {
+        $message .= "<p class='alert alert-danger message'>Bạn đã nhập thiếu mô tả loại sản phẩm</p>";
+    }
+    if (!isset($_FILES['img'])) {
+        $message .= "<p class='alert alert-danger message'>Bạn chưa upload ảnh loại sản phẩm</p>";
+    } else if ($_FILES['img']['error'] > 0) {
+        $message .= "<p class='alert alert-danger message'>Upload ảnh loại sản phẩm bị lỗi</p>";
+    } else {
+        $file_name = $_FILES['img']['name'];
+
+        // lấy phần mở rộng của file
+        //hàm substr là cắt file
+        //hàm strrpos là tìm vị trí xuất hiện cuối cùng của 1 chuỗi trong 1 chuỗi, trả về một số nguyên
+        $file_type = substr($file_name, strrpos($file_name, "."));
+        if (!in_array($file_type, [".jpg", ".png", ".jpeg", "jpe", "gif"])) {
+            $message .= "<p class='alert alert-danger message'>File bạn upload lên không phải là ảnh</p>";
+        } else {
+            $new_file_name = substr($file_name, 0, strrpos($file_name, ".")) . " " . date('d_m_Y H_i_s') . $file_type;
+            $new_file = __DIR__ . '/../public/upload/' . $new_file_name;
+            move_uploaded_file($_FILES['img']['tmp_name'], $new_file);
+            if (!file_exists($new_file)) {//Nếu file không tồn tại
+                $message .= "<p class='alert alert-danger message'>Upload ảnh sản phẩm bị lỗi</p>";
+            }
+        }
+
+    }
+
+    if ($message === "") {
+        if($name_db === $_POST['name'])
+        {
+            $message = "<p class='alert alert-danger message'>Tên loại sản phẩm đã tồn tại</p>";
+        }else if($db->update("type_product",
+            [
+                "name"=>$_POST['name'],
+                "description"=>$_POST['description'],
+            ],
+            "id={$id}"))
+        {
+            $name_db = $_POST['name'];
+            $description_db = $_POST['description'];
+            $message = "<p class='alert alert-success message'>Sửa thành công</p>";
+        }
+    }
+}
+
+?>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="my_pad_top">
+            <a href="list_type_product.php" class="btn btn-primary" >Về trang danh sách loại sản phẩm</a>
+            <a href="index.php" class="btn btn-primary" style="float: right">Về trang chủ</a>
+            <form name="form_product" id="form_product" method="POST" enctype="multipart/form-data">
+                <div id="message">
+                    <?php
+                    if (isset($message)) {
+                        echo $message;
+                    }
+                    ?>
+                </div>
+                <h3>Chỉnh sửa loại sản phẩm</h3>
+
+                <div class="form-group">
+                    <label>Tên</label>
+                    <input type="text" name="name" value="<?php if (isset($name_db)) echo $name_db; ?>"
+                           class="form-control" placeholder="Tên">
+                </div>
+                <div class="form-group">
+                    <label>Mô tả</label>
+                    <input type="text" name="description"
+                           value="<?php if (isset($description_db)) echo $description_db; ?>"
+                           class="form-control" placeholder="Mô tả">
+                </div>
+                <div class="form-group">
+                    <p>Ảnh</p>
+                    <img src="../public/upload/<?php if(isset($image_db)) echo $image_db; ?>" style="width: 25%;">
+                </div>
+
+                <input type="submit" name="btnSubmit" class="btn btn-primary" value="Sửa">
+            </form>
+
+            <script>
+                $(document).ready(function () {
+                    $("#form_product input[name='btnSubmit']").click(function (event) {//bắt sự kiện click vào nút thêm mới
+
+                        event.preventDefault();//ngăn tự động submit form
+
+                        var isValidInput = true;//biến kiểm tra xem các input có hợp lệ không
+                        //Duyệt qua tất cả các input của form
+                        $("#form_product .form-group:has(input[type='text'])").not($(".form-group:has(.selectpicker)")).each(function () {
+
+                            let input = $(this).find("input[type='text']");//Giá trị input
+                            let label = $(this).find("label").text();//Nhãn (nằm trong thẻ label)
+
+                            if (typeof input.val() === "string") {//Nếu trường input là string (text)
+                                $(this).find(".message").remove();//Xóa hết tất cả các thông báo trước
+                                if (input.val() == null || input.val().trim() === "") {//Nếu giá trị input rỗng
+                                    isValidInput = false;
+                                    $(this).append("<p class='alert alert-danger message'>Không thể để trống trường " + label + "</p>");
+                                }
+                            }
+
+                        });
+
+                        //Kiểm tra input ảnh
+                        let inputFile = $('.form-group:has(input[type="file"])');
+                        inputFile.find(".message").remove();//Xóa hết tất cả các thông báo trước
+                        let image = inputFile.find("input[type='file']").val();//Đường dẫn tới ảnh trên máy người dùng
+                        if (image === "") {
+                            isValidInput = false;
+                            inputFile.append("<p class='alert alert-danger message'>Bạn chưa chọn ảnh</p>");
+                        } else {
+                            let file_type = image.substr(image.lastIndexOf("."));//Phần mở rộng của file
+                            if (![".jpg", ".png", ".jpeg", "jpe", "gif"].includes(file_type)) {//Nếu không thuộc 1 trong số này thì không phải là ảnh
+                                isValidInput = false;
+                                inputFile.append("<p class='alert alert-danger message'>File bạn chọn không phải là ảnh</p>");
+                            }
+                        }
+
+                        //Nếu các trường input hợp lệ thì submit form
+                        if (isValidInput) $('#form_product').submit();
+                    });
+                });
+            </script>
+        </div>
+    </div>
+<?php
+require_once __DIR__ . "/includes/footer.php";
+ob_flush();
+?>
