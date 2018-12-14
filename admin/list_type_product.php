94536d447d393fb220a7abdfc6d9ab88b7436300
= <=>
2018-12-14 22:33:09 +0700

    update list_type_product

--- admin/list_type_product.php
+++ admin/list_type_product.php
@@ -53,7 +53,12 @@ $db = db::getInstance();
                 <td><?php echo $obj->id; ?></td>
                 <td><?php echo $obj->name; ?></td>
                 <td><?php echo $obj->description; ?></td>
-                <td><img src="../public/upload/<?php echo $obj->image; ?>" width="100px"></td>
+                <td>
+                    <a href="edit_image_product.php?id=<?php echo $obj->id; ?>" style="font-size: 10px">
+                        <img src="../public/upload/<?php echo $obj->image; ?>" width="100px">
+                        Edit image
+                    </a>
+                </td>
                 <td><a href="edit_type_product.php?id=<?php echo $obj->id ?>"><img width="16px" src="../public/images/icon_edit.png"></a>
                 </td>
                 <td><a href="delete_type_product.php?id=<?php echo $obj->id ?>" onclick="return confirm('Bạn có thực sự muốn xóa không?');"><img width="16px" src="../public/images/icon_delete.png"></a>
