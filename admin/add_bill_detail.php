592bc2ad53bd879a8339cd066f8d5fae74b29cd7
= <=>
2018-12-17 22:18:09 +0700

    update

--- admin/add_bill_detail.php
+++ admin/add_bill_detail.php
@@ -49,13 +49,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <div class="col-lg-12 col-sm-12 col-xs-12 col-md-12" id="my_pad_top">
         <a href="list_bill_detail.php" class="btn btn-primary" >Về trang danh sách hóa đơn chi tiết</a>
         <a href="index.php" class="btn btn-primary" style="float: right">Về trang chủ</a>
-        <h3>Thêm mới hóa đơn chi tiết</h3>
+
         <form id="frm_add" method="POST" name="frm_add" enctype="multipart/form-data">
             <?php
             if (isset($message)) {
                 echo $message;
             }
             ?>
+            <h3>Thêm mới hóa đơn chi tiết</h3>
             <div class="form-group">
                 <label>Mã hóa đơn</label>
                 <input type="text" hidden name="mahd" value="">
