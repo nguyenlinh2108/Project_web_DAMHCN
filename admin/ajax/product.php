e2bdf382d1b163402d5021ed48df445786649a9a
Long <alphahoai@gmail.com>
2018-12-18 21:12:48 +0700

    viết gọn lại code sử dụng hàm lưu file ảnh đã upload lên

--- admin/ajax/product.php
+++ admin/ajax/product.php
@@ -6,6 +6,8 @@
  */
 
 require_once __DIR__ . "/../../db/db.php";
+require_once __DIR__ . "/../../utils/echojsonmessage.php";
+require_once __DIR__ . "/../../utils/uploadimage.php";
 
 $db = db::getInstance();
 
@@ -25,53 +27,19 @@ switch ($_GET['type']) {
                 echo_error("Mising id");
             }
 
+            $upload_status = saveImage('img',   __DIR__ . '/../../public/upload/product/');
+            if($upload_status['success']){
 
-            if (!empty($_FILES['img'])) {
-                $file_name = $_FILES['img']['name'];
-                // lấy phần mở rộng của file
-                //hàm substr là cắt file
-                //hàm strrpos là tìm vị trí xuất hiện cuối cùng của 1 chuỗi trong 1 chuỗi, trả về một số nguyên
-                $file_type = substr($file_name, strrpos($file_name, "."));
-                if (!in_array(strtolower($file_type), [".jpg", ".png", ".jpeg", ".jpe", ".gif"])) {
-                    echo_error("Chỉ được upload file ảnh ($file_type)" );
+                if (!$db->select_one("SELECT * FROM product WHERE id = {$id}")) {
+                    echo_error("Sản phẩm không tồn tại");
                 } else {
-                    $new_file_name = substr($file_name, 0, strrpos($file_name, ".")) . " " . date('d_m_Y H_i_s') . $file_type;
-                    $new_file = __DIR__ . '/../../public/upload/product/' . $new_file_name;
-
-                    if (move_uploaded_file($_FILES['img']['tmp_name'], $new_file)) {
-
-                        if (!$db->select_one("SELECT * FROM product WHERE id = {$id}")) {
-                            echo_error("Sản phẩm không tồn tại");
-                        } else {
-                            if ($db->update("product", ["image" => $new_file_name], "id = {$id}")) {
-                                echo_success($new_file_name);
-                            } else echo_error("Không thể sửa ảnh");
-                        }
-
-                    } else {
-                        echo_error();
-                    }
+                    if ($db->update("product", ["image" => $upload_status['new_file_name']], "id = {$id}")) {
+                        echo_success($upload_status['new_file_name']);
+                    } else echo_error("Không thể sửa ảnh");
                 }
+
             } else {
-                echo_error("Mising file");
+                echo_error($upload_status['message']);
             }
         }
-}
-
-function echo_message($success, $message = null)
-{
-    $res = ['success' => $success];
-    if ($message != null) $res['message'] = $message;
-    echo json_encode($res);
-    exit();
-}
-
-function echo_success($message = null)
-{
-    echo_message(true, $message);
-}
-
-function echo_error($message = null)
-{
-    echo_message(false, $message);
 }
