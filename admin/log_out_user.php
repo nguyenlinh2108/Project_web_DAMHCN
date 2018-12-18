de10bcadca26451d2e16616c29fc81d7794fb08e
Long <alphahoai@gmail.com>
2018-12-18 15:32:10 +0700

    update logout user

--- admin/log_out_user.php
+++ admin/log_out_user.php
@@ -5,20 +5,10 @@
  * Date: 12/15/2018
  * Time: 4:41 PM
  */
+session_start();
 
-if(isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT,array('min_range'=>1)))
-{
-    $id = $_GET['id'];
-    if(isset($_SESSION['login'][$id]))
-    {
-//
-        unset($_SESSION['login']);
-        header("location: /admin/login.php");
-//        header('Location:/admin/login.php');
-    }
-}else {
-    header('Location: index.php');
-    exit();
-}
+session_destroy();
+session_unset();
 
-?>
+header("location: /admin/login.php");
+exit;
