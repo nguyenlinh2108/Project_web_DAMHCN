1a1a994c19dd572cc73b5b09e4e2a51582fb72ca
= <=>
2018-12-21 18:15:39 +0700

    hien thi anh cua san pham trong trang chu

--- includes/products.php
+++ includes/products.php
@@ -1,3 +1,10 @@
+<?php
+require_once __DIR__ . "/../db/db.php";
+$db = db::getInstance();
+if ($db->select("SELECT * FROM product")) {
+    $products = $db->getResult();//Danh sách sản phẩm
+}
+?>
 <div class="container-fluid products">
 	<div class="container">
 		<div class="row">
@@ -10,96 +17,58 @@
 		</div>
 		<div class="row">
 			<section class="center1 slider hidden-sm-down">
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-1.png">
-						<p>Artisan chocolate box</p>
-						<span class="price-product">made to order</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-2.png">
-						<p>Chocolate truffle box</p>
-						<span class="price-product">$30.00</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-3.png">
-						<p>Chocolate bar</p>
-						<span class="price-product">$7.50</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-4.png">
-						<p>Baking chocolate 4 packs</p>
-						<span class="price-product">$33.00</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-5.png">
-						<p>Chef collection</p>
-						<span class="price-product">$9.00</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				
+                <?php
+                if(isset($products) && is_array($products))
+                {
+                    foreach ($products as $product)
+                    {
+                        if($product->soluong >0) {
+                            ?>
+                            <div class="item">
+                                <div class="bd-item">
+                                    <img src="../public/upload/product/<?php echo $product->image; ?>" height="303px">
+                                    <p><?php echo $product->name; ?></p>
+                                    <span class="price-product"><?php echo $product->unit_price; ?></span>
+                                    <a href="add-to-cart.php?id=<?php echo $product->id; ?>"
+                                       class="btn btn-chocolate no-bg">see product <span
+                                                class="fa fa-chevron-circle-right"></span></a>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    }
+                }
+                ?>
 			</section>
 
 			<section class="center2 slider hidden-sm-up">
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-1.png">
-						<p>Artisan chocolate box</p>
-						<span class="price-product">made to order</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-2.png">
-						<p>Chocolate truffle box</p>
-						<span class="price-product">$30.00</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-3.png">
-						<p>Chocolate bar</p>
-						<span class="price-product">$7.50</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-4.png">
-						<p>Baking chocolate 4 packs</p>
-						<span class="price-product">$33.00</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				<div class="item">
-					<div class="bd-item">
-						<img src="public/images/product-5.png">
-						<p>Chef collection</p>
-						<span class="price-product">$9.00</span>
-						<a href="add-to-cart.html" class="btn btn-chocolate no-bg">see product <span class="fa fa-chevron-circle-right"></span></a>
-					</div>
-				</div>
-				
+                <?php
+                if(isset($products) && is_array($products))
+                {
+                    foreach ($products as $product)
+                    {
+                        if($product->soluong > 0) {
+                            ?>
+                            <div class="item">
+                                <div class="bd-item">
+                                    <img src="../public/upload/product/<?php echo $product->image; ?>" height="303px">
+                                    <p><?php echo $product->name; ?></p>
+                                    <span class="price-product"><?php echo $product->unit_price; ?></span>
+                                    <a href="add-to-cart.php?id=<?php echo $product->id; ?>"
+                                       class="btn btn-chocolate no-bg">see product <span
+                                                class="fa fa-chevron-circle-right"></span></a>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    }
+                }
+                ?>
 			</section>
 
 			
 			<div class="col-md-4 push-md-4 text-xs-center see-all">
-				<a href="products.html" class="btn btn-chocolate">see all the pods products <span class="fa fa-chevron-circle-right"></span></a>
+				<a href="../products.php" class="btn btn-chocolate">see all the pods products <span class="fa fa-chevron-circle-right"></span></a>
 			</div>
 		</div>
 	</div>
