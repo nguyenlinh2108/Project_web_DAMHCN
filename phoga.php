dab876cf28dbe5d6893e2af85f21b1b1d7ce37da
= <=>
2018-12-21 18:55:48 +0700

    update hien thi link-menu trong phoga.php

--- phoga.php
+++ phoga.php
@@ -57,59 +57,7 @@ if ($db->select("SELECT * FROM type_product")) {
 
 
 
-	<div class="container-fluid link-menu">
-		<div class="row">
-			<div class="col-md-3 col-sm-12 col-xs-12 up-one text-xs-center">
-				<a class="the-pods-footer" href="index.php">
-					<img src="public/images/three-pods-logo.png" alt="">
-					<p>THE PODS</p>
-				</a>
-			</div>
-			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-				<p class="title-footer">ABOUT US</p>
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="ours_tory.php">Our story</a></li>
-					<li><a href="products.php">Products</a></li>
-<!--					<li><a href="press.php">Press</a></li>-->
-<!--					<li><a href="recipes.php">Recipes</a></li>-->
-					<li><a href="phoga.php">Gallery</a></li>
-				</ul>
-			</div>
-			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-				<p class="title-footer">PRODUCTS</p>
-				<ul>
-                    <?php
-                    if(isset($types) && is_array($types))
-                    {
-                        foreach ($types as $type)
-                        {
-                            ?>
-                            <li><a href=""><?php echo $type->name; ?></a></li>
-                            <?php
-                        }
-                    }
-                    ?>
-				</ul>
-			</div>
-			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-				<p class="title-footer">SUPPORT</p>
-				<ul>
-					<li><a href="contact.php">Contact</a></li>
-<!--					<li><a href="latest-news.php">Latest News</a></li>-->
-<!--					<li><a href="">Privacy policy</a></li>-->
-<!--					<li><a href="">Gift cards</a></li>-->
-<!--					<li><a href="">FAQ</a></li>-->
-				</ul>
-			</div>
-<!--			<div class="col-md-3 col-sm-3 col-xs-6 up-one">-->
-<!--				<p class="title-footer">SUBSCRIBE OUR NEWSLETTER!</p>-->
-<!--				<p class="details-footer">Let us update to you our news & promotion. <br> 25% discount code for any new subscription.</p>-->
-<!--				<input type="email" placeholder="Enter your e-mail here" class="d-block mb-2">-->
-<!--				<a href="" type="submit" class="btn btn-chocolate ">SUBSCRIBE <span class="fa fa-chevron-circle-right"></span></a>-->
-<!--			</div>-->
-		</div><!-- end row -->
-	</div><!-- end ... -->
+<?php require_once __DIR__ . "/includes/link-menu.php"; ?>
 
 	<footer>
 		<div class="container-fluid">
