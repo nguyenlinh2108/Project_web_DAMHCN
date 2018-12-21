e73597eddcf79bc508d8071d12a82f0113e2ce44
= <=>
2018-12-21 18:59:06 +0700

    update

--- includes/footer.php
+++ includes/footer.php
@@ -4,19 +4,19 @@
 			<div class="row">
 				<div class="col-md-10 push-md-1 col-xs-10 push-xs-1 up hidden-xs-down">
 					<ul>
-						<li class="active"><a href="index.html">Home</a></li>
-						<li><a href="our_story.html">Our Story</a></li>
-						<li><a href="products.html">Products</a></li>
+						<li class="active"><a href="index.php">Home</a></li>
+						<li><a href="our_story.php">Our Story</a></li>
+						<li><a href="products.php">Products</a></li>
 						<!-- <li><a href="press.html">Press</a></li> -->
-						<li><a href="recipes.html">Recipes</a></li>
-						<li><a href="contact.html">Contact</a></li>
-						<li><a href="lastes_news.html">Latest News</a></li>
-						<li><a href="phoga.html">Gallery</a></li>
+<!--						<li><a href="recipes.html">Recipes</a></li>-->
+						<li><a href="contact.php">Contact</a></li>
+<!--						<li><a href="lastes_news.html">Latest News</a></li>-->
+						<li><a href="phoga.php">Gallery</a></li>
 					</ul>
 				</div>
 				<div class="col-md-12 down text-xs-center">
 					<img src="public/images/three-pods-logo.png" alt="">
-					<p>&copy; Copyright 2017 Lilystreat.  All rights reserved.</p>
+					<p>&copy; THE PODS.</p>
 				</div>
 			</div><!-- end row -->
 		</div><!-- end container -->
