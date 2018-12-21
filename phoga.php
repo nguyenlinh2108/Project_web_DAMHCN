a49d0d3c9037665cfeff174959c6aaf47884864a
= <=>
2018-12-21 18:28:44 +0700

    upload file photga.php

--- phoga.php
+++ phoga.php
@@ -1,5 +1,11 @@
-<?php include('includes/header.php'); ?>
-
+<?php
+require_once __DIR__ . "/includes/header.php";
+require_once __DIR__ . "/db/db.php";
+$db = db::getInstance();
+if ($db->select("SELECT * FROM type_product")) {
+    $types = $db->getResult();//Danh sách các loại sản phẩm
+}
+?>
 	<div class="container-fluid pho-gal">
 		<div class="container">
 			<div class="row">
@@ -7,10 +13,17 @@
 					<div class="titles text-xs-center">
 						<h2><span class="cho-left"></span>  Photo Gallery  <span class="cho-right"></span></h2>
 						<ul>
-							<li><a href="">Chocolate</a></li>
-							<li><a href="">Store</a></li>
-							<li><a href="">Classes</a></li>
-							<li><a href="">The making</a></li>
+                            <?php
+                            if(isset($types) && is_array($types))
+                            {
+                            foreach ($types as $type)
+                            {
+                            ?>
+							<li><a href=""><?php echo $type->name; ?></a></li>
+                            <?php
+                            }
+                            }
+                            ?>
 						</ul>
 					</div>
 				</div>
@@ -21,116 +34,22 @@
 	<div class="container-fluid">
 		<div class="container">
 			<div class="photos">
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-1.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo">
-					<figure>
-						<img src="public/images/product-2.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-3.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-4.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo">
-					<figure>
-						<img src="public/images/product-5.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-5.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-7.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-8.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-9.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-10.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-11.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-12.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-13.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-14.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-15.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/product-16.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo">
-					<figure>
-						<img src="public/images/photo-1.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w2">
-					<figure>
-						<img src="public/images/photo-2.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo">
-					<figure>
-						<img src="public/images/photo-3.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/photo-4.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/photo-5.png" alt="" class="">
-					</figure>
-				</div>
-				<div class="photo photo-w3">
-					<figure>
-						<img src="public/images/photo-6.png" alt="" class="">
-					</figure>
-				</div>
+                <?php
+                if(isset($types) && is_array($types))
+                {
+                    foreach ($types as $type)
+                    {
+                        ?>
+                        <div class="photo photo-w3">
+                            <figure>
+                                <img src="public/upload/type_product/<?php echo $type->image; ?>" alt="" class="">
+                            </figure>
+                        </div>
+                        <?php
+                    }
+                }
+                ?>
+
 			</div>
 		</div>
 	</div>
@@ -141,7 +60,7 @@
 	<div class="container-fluid link-menu">
 		<div class="row">
 			<div class="col-md-3 col-sm-12 col-xs-12 up-one text-xs-center">
-				<a class="the-pods-footer" href="index.html">
+				<a class="the-pods-footer" href="index.php">
 					<img src="public/images/three-pods-logo.png" alt="">
 					<p>THE PODS</p>
 				</a>
@@ -149,40 +68,46 @@
 			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
 				<p class="title-footer">ABOUT US</p>
 				<ul>
-					<li><a href="index.html">Home</a></li>
-					<li><a href="ours_tory.html">Our story</a></li>
-					<li><a href="products.html">Products</a></li>
-					<li><a href="press.html">Press</a></li>
-					<li><a href="recipes.html">Recipes</a></li>
-					<li><a href="phoga.html">Gallery</a></li>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="ours_tory.php">Our story</a></li>
+					<li><a href="products.php">Products</a></li>
+<!--					<li><a href="press.php">Press</a></li>-->
+<!--					<li><a href="recipes.php">Recipes</a></li>-->
+					<li><a href="phoga.php">Gallery</a></li>
 				</ul>
 			</div>
 			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
 				<p class="title-footer">PRODUCTS</p>
 				<ul>
-					<li><a href="">Chocolates</a></li>
-					<li><a href="">Handmade chocolate</a></li>
-					<li><a href="">Baking & cooking chocolate</a></li>
-					<li><a href="">Season specialties</a></li>
-					<li><a href="">Gift sets</a></li>
+                    <?php
+                    if(isset($types) && is_array($types))
+                    {
+                        foreach ($types as $type)
+                        {
+                            ?>
+                            <li><a href=""><?php echo $type->name; ?></a></li>
+                            <?php
+                        }
+                    }
+                    ?>
 				</ul>
 			</div>
 			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
 				<p class="title-footer">SUPPORT</p>
 				<ul>
-					<li><a href="contact.html">Contact</a></li>
-					<li><a href="latest-news.html">Latest News</a></li>
-					<li><a href="">Privacy policy</a></li>
-					<li><a href="">Gift cards</a></li>
-					<li><a href="">FAQ</a></li>
+					<li><a href="contact.php">Contact</a></li>
+<!--					<li><a href="latest-news.php">Latest News</a></li>-->
+<!--					<li><a href="">Privacy policy</a></li>-->
+<!--					<li><a href="">Gift cards</a></li>-->
+<!--					<li><a href="">FAQ</a></li>-->
 				</ul>
 			</div>
-			<div class="col-md-3 col-sm-3 col-xs-6 up-one">
-				<p class="title-footer">SUBSCRIBE OUR NEWSLETTER!</p>
-				<p class="details-footer">Let us update to you our news & promotion. <br> 25% discount code for any new subscription.</p>
-				<input type="email" placeholder="Enter your e-mail here" class="d-block mb-2">
-				<a href="" type="submit" class="btn btn-chocolate ">SUBSCRIBE <span class="fa fa-chevron-circle-right"></span></a>
-			</div>
+<!--			<div class="col-md-3 col-sm-3 col-xs-6 up-one">-->
+<!--				<p class="title-footer">SUBSCRIBE OUR NEWSLETTER!</p>-->
+<!--				<p class="details-footer">Let us update to you our news & promotion. <br> 25% discount code for any new subscription.</p>-->
+<!--				<input type="email" placeholder="Enter your e-mail here" class="d-block mb-2">-->
+<!--				<a href="" type="submit" class="btn btn-chocolate ">SUBSCRIBE <span class="fa fa-chevron-circle-right"></span></a>-->
+<!--			</div>-->
 		</div><!-- end row -->
 	</div><!-- end ... -->
 
@@ -191,19 +116,19 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12 down text-xs-center">
-						<p>&copy; Copyright 2017 Lilystreat.  All rights reserved.</p>
+						<p>&copy; THE PODS</p>
 					</div>
 				</div><!-- end row -->
 			</div>
 		</div>
 	</footer><!-- end ... -->
 
-	<div class="connect">
-		<a href="#"><span class="fa fa-facebook"></span></a>
-		<a href="#"><span class="fa fa-twitter"></span></a>
-		<a href="#"><span class="fa fa-google-plus"></span></a>
-		<a href="#"><span class="fa fa-instagram"></span></a>
-	</div>
+<!--	<div class="connect">-->
+<!--		<a href="#"><span class="fa fa-facebook"></span></a>-->
+<!--		<a href="#"><span class="fa fa-twitter"></span></a>-->
+<!--		<a href="#"><span class="fa fa-google-plus"></span></a>-->
+<!--		<a href="#"><span class="fa fa-instagram"></span></a>-->
+<!--	</div>-->
 
 	<div id="toTop">
 		<span class="fa fa-chevron-up"></span>
