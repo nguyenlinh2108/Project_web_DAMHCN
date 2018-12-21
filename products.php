1f565db384bb1737ede70b59b178e05f8a6a9077
= <=>
2018-12-21 19:00:52 +0700

    update link-menu trong product.php

--- products.php
+++ products.php
@@ -114,65 +114,7 @@ if ($db->select("SELECT * FROM product")) {
 		</div>
 	</div> -->
 
-	<div class="container-fluid link-menu">
-		<div class="row">
-			<div class="col-md-3 col-sm-12 col-xs-12 up-one text-xs-center">
-				<a class="the-pods-footer" href="index.php">
-					<img src="public/images/three-pods-logo.png" alt="">
-					<p>THE PODS</p>
-				</a>
-			</div>
-			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-				<p class="title-footer">ABOUT US</p>
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="ours_tory.php">Our story</a></li>
-					<li><a href="products.php">Products</a></li>
-					<li><a href="press.php">Press</a></li>
-					<li><a href="recipes.php">Recipes</a></li>
-					<li><a href="phoga.php">Gallery</a></li>
-				</ul>
-			</div>
-			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-				<p class="title-footer">PRODUCTS</p>
-				<ul>
-					<li><a href="">Chocolates</a></li>
-					<li><a href="">Handmade chocolate</a></li>
-					<li><a href="">Baking & cooking chocolate</a></li>
-					<li><a href="">Season specialties</a></li>
-					<li><a href="">Gift sets</a></li>
-				</ul>
-			</div>
-			<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-				<p class="title-footer">SUPPORT</p>
-				<ul>
-					<li><a href="contact.php">Contact</a></li>
-					<li><a href="latest-news.php">Latest News</a></li>
-					<li><a href="">Privacy policy</a></li>
-					<li><a href="">Gift cards</a></li>
-					<li><a href="">FAQ</a></li>
-				</ul>
-			</div>
-<!--			<div class="col-md-3 col-sm-3 col-xs-6 up-one">-->
-<!--				<p class="title-footer">SUBSCRIBE OUR NEWSLETTER!</p>-->
-<!--				<p class="details-footer">Let us update to you our news & promotion. <br> 25% discount code for any new subscription.</p>-->
-<!--				<input type="email" placeholder="Enter your e-mail here" class="d-block mb-2">-->
-<!--				<a href="" type="submit" class="btn btn-chocolate ">SUBSCRIBE <span class="fa fa-chevron-circle-right"></span></a>-->
-<!--			</div>-->
-		</div><!-- end row -->
-	</div><!-- end ... -->
-
-	<footer>
-		<div class="container-fluid">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12 down text-xs-center">
-						<p>&copy; Copyright 2017 Lilystreat.  All rights reserved.</p>
-					</div>
-				</div><!-- end row -->
-			</div>
-		</div>
-	</footer><!-- end ... -->
+<?php require_once __DIR__ . "/includes/link-menu.php"; ?>
 
 <!--	<div class="connect">-->
 <!--		<a href="#"><span class="fa fa-facebook"></span></a>-->
