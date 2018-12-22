0d660ab065ea5eae3df2b6b7f33aa0d21faf7612
= <=>
2018-12-22 13:24:36 +0700

    update

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 				<div class="col-sm-6 push-sm-3 log">
 					<div class="card card-block cart-height col-sm-10 push-sm-1">
 						<h3 class="card-title text-xs-center">Login</h3>
-						<form method="POST" action="inf-acc.html">
+						<form method="POST" action="inf-acc.php">
 							<fieldset class="form-group">
 								<label for="InputEmail">*Email Address:</label>
 								<input required type="text" class="form-control" id="InputEmail" placeholder="john@gmail.com">
@@ -59,7 +59,7 @@
 				<div class="col-sm-8 push-sm-2">
 					<div class="card card-block cart-height">
 						<h3 class="card-title text-xs-center">New Member Registration</h3>
-						<form method="POST" action="inf-acc.html">
+						<form method="POST" action="inf-acc.php">
 							<div class="row">
 								<fieldset class="form-group col-sm-6">
 									<label for="InputFirstName">*First Name</label>
@@ -138,72 +138,12 @@
 	
 
 
-	<div class="container-fluid link-menu">
-			<div class="row">
-				<div class="col-md-3 col-sm-12 col-xs-12 up-one text-xs-center">
-					<a class="the-pods-footer" href="index.html">
-						<img src="public/images/three-pods-logo.png" alt="">
-						<p>THE PODS</p>
-					</a>
-				</div>
-				<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-					<p class="title-footer">ABOUT US</p>
-					<ul>
-						<li><a href="index.html">Home</a></li>
-						<li><a href="ours_tory.html">Our story</a></li>
-						<li><a href="products.html">Products</a></li>
-						<li><a href="press.html">Press</a></li>
-						<li><a href="recipes.html">Recipes</a></li>
-						<li><a href="phoga.html">Gallery</a></li>
-					</ul>
-				</div>
-				<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-					<p class="title-footer">CHOCOLATE</p>
-					<ul>
-						<li><a href="">Chocolate</a></li>
-						<li><a href="">Handmade chocolate</a></li>
-						<li><a href="">Baking & cooking chocolate</a></li>
-						<li><a href="">Season specialties</a></li>
-						<li><a href="">Gift sets</a></li>
-					</ul>
-				</div>
-				<div class="col-md-2 col-sm-3 col-xs-6 up-one">
-					<p class="title-footer">SUPPORT</p>
-					<ul>
-						<li><a href="contact.html">Contact</a></li>
-						<li><a href="latest-news.html">Latest News</a></li>
-						<li><a href="">Privacy policy</a></li>
-						<li><a href="">Gift cards</a></li>
-						<li><a href="">FAQ</a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 col-sm-3 col-xs-6 up-one">
-					<p class="title-footer">SUBSCRIBE OUR NEWSLETTER!</p>
-					<p class="details-footer">Let us update to you our news & promotion. <br> 25% discount code for any new subscription.</p>
-					<input type="email" placeholder="Enter your e-mail here" class="d-block mb-2">
-					<a href="" type="submit" class="btn btn-chocolate ">SUBSCRIBE <span class="fa fa-chevron-circle-right"></span></a>
-				</div>
-			</div><!-- end row -->
-	</div><!-- end ... -->
 
-	<footer>
-		<div class="container-fluid">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12 down text-xs-center">
-						<p>&copy; Copyright 2017 Lilystreat.  All rights reserved.</p>
-					</div>
-				</div><!-- end row -->
-			</div>
-		</div>
-	</footer><!-- end ... -->
 
-	<div class="connect">
-		<a href="#"><span class="fa fa-facebook"></span></a>
-		<a href="#"><span class="fa fa-twitter"></span></a>
-		<a href="#"><span class="fa fa-google-plus"></span></a>
-		<a href="#"><span class="fa fa-instagram"></span></a>
-	</div>
+<?php
+require_once __DIR__ . "/includes/link-menu.php";
+//require_once __DIR__ . "/includes/connect.php";
+?>
 
 	<div id="toTop">
 		<span class="fa fa-chevron-up"></span>
