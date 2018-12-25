30a0f82a1328f9058f475dfc51c2bb609cd88042
= <=>
2018-12-25 21:53:05 +0700

    them file tim kiem san pham

---
+++ search.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linh nguyễn
+ * Date: 12/25/2018
+ * Time: 5:43 PM
+ */
+ob_start();
+require_once __DIR__ . "/includes/header.php";
+require_once __DIR__ . "/db/db.php";
+$db = db::getInstance();
+$mess = "";
+if(isset($_GET['searchcontent']) && $_GET['searchcontent']!==""){
+    $description = $_GET['searchcontent'];
+    if($db->select("SELECT * FROM product WHERE name LIKE '%".$description."%' OR description  LIKE '%".$description."%'"))
+    {
+        $productsearchs = $db->getResult();
+        $mess = "Kết quả tìm kiếm với từ khóa: ".$description;
+    }else{
+        $mess = "Không có kết quả nào";
+    }
+}else{
+    header("Location: index.php");
+    exit();
+}
+?>
+<div class="container-fluid products-shop">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 col-sm-9">
+                <div class="grid row show-products">
+                    <div class="col-md-12 col-sm-12 col-xs-12 mb-3">
+                      <?php
+                      if($mess !== ""){
+                          echo $mess;
+                      }
+                      ?>
+                    </div>
+
+                    <?php
+                    if(isset($productsearchs) && is_array($productsearchs))
+                    {
+                        foreach ($productsearchs as $productsearch)
+                        {
+                            ?>
+                            <div class="element-item button<?= $productsearch->id ?> ?> col-md-4 col-sm-6 col-xs-6">
+                                <figure class="product-box text-xs-center">
+                                    <div class="quick-view">
+                                        <img class="img-fluid"
+                                             src="public/upload/product/<?= $productsearch->image ?>"
+                                             alt="">
+                                        <div class="bg-gray"></div>
+                                        <a href="product.php?id=<?= $productsearch->id ?>"
+                                           class="btn btn-chocolate cd-trigger"> Xem thêm <span
+                                                class="fa fa-chevron-circle-right"></span></a>
+                                    </div>
+                                    <figcaption><p><?php echo $productsearch->name ?></p><span
+                                            class="price-product"><?= $productsearch->unit_price ?>
+                                            đ</span></figcaption>
+                                </figure>
+                            </div>
+                            <?php
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div><!-- end container -->
+</div><!-- end ... -->
+
+<?php
+require_once __DIR__ . "/includes/link-menu.php";
+ob_flush();
+?>
+<div id="toTop">
+    <span class="fa fa-chevron-up"></span>
+</div>
+<div id="toDown">
+    <span class="fa fa-chevron-down"></span>
+</div>
