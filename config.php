7cce4e5d7776146eeb7c2158bc442b4553a9fce6
= <=>
2018-12-25 17:03:20 +0700

    remove Goolge Api key

--- config.php
+++ config.php
@@ -15,6 +15,3 @@ $CONFIG_DATABASE['USERNAME'] = 'root';
 $CONFIG_DATABASE['PASSWORD'] = "";
 
 define("ADMIN_SALT_PASSWORD", "XinchaoVietNam2018");
-
-define("GOOGLE_OAUTH_CLIENT_ID","391241154435-343jq3drg9v10dripnenfmauektj3qm5.apps.googleusercontent.com");
-define("GOOGLE_OAUTH_CLIENT_SECRET","H00QQTX-KBHzgsDfpaMUG2hY");
