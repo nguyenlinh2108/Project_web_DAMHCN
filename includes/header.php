34ee315e55b2fdfe21096c3550ae3df69126542a
= <=>
2018-12-25 21:52:18 +0700

    update search

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,20 @@ $db = db::getInstance();
 if (isset($_COOKIE['product_cart'])) {
     $product_cards = json_decode($_COOKIE['product_cart']);
 }
+
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    if(isset($_POST['search-content']) && $_POST['search-content'] !== "")
+    {
+        $searchcontent = $_POST['search-content'];
+        header("Location: search.php?searchcontent=$searchcontent");
+        exit();
+    }else{
+        header("Location: index.php");
+        exit();
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -67,8 +81,8 @@ if (isset($_COOKIE['product_cart'])) {
 
                 <li class="nav-item search">
                     <a class="nav-link" href="javascript:void(0)"><span class="fa fa-search"></span> Search</a>
-                    <form class="search-form">
-                        <input type="search" placeholder="Search here..." class="none-outline form-control">
+                    <form class="search-form" name="search-form" method="POST">
+                        <input name="search-content" type="search" placeholder="Search here..." class="none-outline form-control">
                         <button type="submit" class="btn btn-search"><span class="fa fa-search"></span></button>
                     </form>
                 </li>
@@ -145,8 +159,8 @@ if (isset($_COOKIE['product_cart'])) {
 
                 <li class="nav-item search">
                     <a class="nav-link" href="javascript:void(0)"><span class="fa fa-search"></span> Search</a>
-                    <form class="search-form">
-                        <input type="search" placeholder="Search here..." class="none-outline form-control">
+                    <form class="search-form" name="search-form" method="POST">
+                        <input name="search-content" type="search" placeholder="Search here..." class="none-outline form-control">
                         <button type="submit" class="btn btn-search"><span class="fa fa-search"></span></button>
                     </form>
                 </li>
